464fd6b38b25b00b7abffbd72e685989febfc0d2
seva.ask <seva.ask@53d31245-e03d-a892-6511-c4cfca5251c2>
2010-03-29 20:42:15 +0000

    SND\seva00_cp: vs 2010 rc

---
+++ trunk/QReal/Types/TypeManager.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Reflection;
+using System.IO;
+using System.Collections.Generic;
+using QReal.Web.Assemblies;
+using System.Diagnostics;
+using ObjectTypes;
+using System.Threading;
+
+namespace QReal.Types
+{
+    public delegate void LoadingComplete();
+
+    public class TypeManager
+    {
+        public Dictionary<string, DiagramWithTypes> Objects { get; private set; }
+
+        private static TypeManager instance = new TypeManager();
+
+        public static TypeManager Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        private TypeManager()
+        {
+            Objects = new Dictionary<string, DiagramWithTypes>();
+            AssembliesContext assembliesContext = new AssembliesContext();
+            assembliesContext.GetAssemblies(operation =>
+            {
+                mReady = operation.Value.Length;
+                foreach (string file in operation.Value)
+                {
+                    GetSerializedAssembly(file);
+                }
+            }, null);
+        }
+
+        private int mReady = -1;
+
+        private void SetReady(int value)
+        {
+            mReady = value;
+        }
+
+        private void DecreaseReady()
+        {
+            mReady--;
+            if (mReady == 0)
+            {
+                foreach (LoadingComplete action in actions)
+                {
+                    action();
+                }
+            }
+        }
+
+        private void GetSerializedAssembly(string file)
+        {
+            AssembliesContext assembliesContext = new AssembliesContext();
+            assembliesContext.GetSerialiazedAssembly(file, operation =>
+            {
+                MemoryStream ms = new MemoryStream(operation.Value);
+                AssemblyPart part = new AssemblyPart();
+                Assembly assembly = part.Load(ms);
+                Type[] availableTypes = assembly.GetTypes();
+                Type diagram = null;
+                Dictionary<string, Type> types = new Dictionary<string, Type>();
+                foreach (Type type in availableTypes)
+                {
+                    if (type.GetInterface(typeof(IDiagram).FullName, false) != null)
+                    {
+                        diagram = type;
+                    }
+                    if (type.IsSubclassOf(typeof(ObjectType)))
+                    {
+                        ObjectType tmpType = (ObjectType)Activator.CreateInstance(type);
+                        types[tmpType.TypeName] = type;
+                    }
+                }
+                string diagramName = ((IDiagram)Activator.CreateInstance(diagram)).Name;
+                Objects[diagramName] = new DiagramWithTypes(diagram, types);
+                DecreaseReady();
+            }, null);
+        }
+
+        public void Request(LoadingComplete action)
+        {
+            if (mReady != 0)
+            {
+	            actions.Add(action);
+            }
+            else
+            {
+                action();
+            }
+        }
+
+        List<LoadingComplete> actions = new List<LoadingComplete>();
+    }
+}
