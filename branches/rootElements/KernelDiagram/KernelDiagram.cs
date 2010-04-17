38a3956cacfc6b2cdf08ff68548c69d9e96615da
seva.ask <seva.ask@53d31245-e03d-a892-6511-c4cfca5251c2>
2010-04-17 23:09:22 +0000

    root elements. all but saving and rejecting

---
+++ branches/rootElements/KernelDiagram/KernelDiagram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ObjectTypes;
+
+namespace KernelDiagram
+{
+    public class KernelDiagram : IDiagram
+    {
+        public string Name
+        {
+            get { return "Kernel Diagram"; }
+        }
+    }
+}
---
+++ branches/rootElements/QReal/Controls/IdToSelectedConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Data;
+using ObjectTypes;
+
+namespace QReal.Controls
+{
+    public class IdToSelectedConverter : IValueConverter
+    {
+        private ObjectType objectType;
+
+        public IdToSelectedConverter(ObjectType objectType)
+        {
+            this.objectType = objectType;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value == objectType.DataContext;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
